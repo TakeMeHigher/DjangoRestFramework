5bbbb9da04f8f9cdee8ddcfc9016cf219769256a
TakeMeHiger <492745473@qq.com>
2018-02-06 21:04:05 +0800

    创建MyAuthentication用于认证

---
+++ app04/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import BasePermission
+from rest_framework.throttling import SimpleRateThrottle
+from rest_framework import exceptions
+
+from app01 import models
+
+# Create your views here.
+class MyAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token=request.query_params.get('token')
+        user=models.Userinfo.objects.filter(token=token).first()
+        if user:
+            return (user.name,user)
+        return None
+
